77bdc5805c46c72b4172a8964c48e27ba0210c4f
Aman N <aman.narayan1998@gmail.com>
2021-06-06 20:21:54 +0530

    Updated

--- client/src/components/PostDetails/PostDetails.js
+++ client/src/components/PostDetails/PostDetails.js
@@ -73,15 +73,6 @@ const PostDetails = () => {
           <Typography variant="body1">
             {dayjs(post.createdAt).fromNow()}
           </Typography>
-          <Divider style={{ margin: "20px 0" }} />
-          <Typography variant="body1">
-            <strong>Realtime Chat - coming soon!</strong>
-          </Typography>
-          <Divider style={{ margin: "20px 0" }} />
-          <Typography variant="body1">
-            <strong>Comments - coming soon!</strong>
-          </Typography>
-          <Divider style={{ margin: "20px 0" }} />
         </div>
         <div className={classes.imageSection}>
           <img
